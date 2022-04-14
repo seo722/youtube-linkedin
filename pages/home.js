cd7b346ae5c7b4a02d442d24d60710637190dbf5
seo722 <95078290+seo722@users.noreply.github.com>
2022-04-14 15:08:20 +0900

    header

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,38 @@
-function home() {
-  return <div>home</div>;
+import Image from "next/image";
+import HeaderLink from "../components/HeaderLink";
+import ExploreIcon from "@mui/icons-material/Explore";
+import GroupIcon from "@mui/icons-material/Group";
+import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import Example from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <header className="flex justify-around items-center py-4">
+        <div className="relative w-36 h-10">
+          <Image src="http://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+        </div>
+        <div className="flex items-center sm:divide-x divide-gray-300">
+          <div className="hidden sm:flex space-x-8 pr-4">
+            <HeaderLink Icon={ExploreIcon} text="Discover" />
+            <HeaderLink Icon={GroupIcon} text="People" />
+            <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
+            <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
+          </div>
+          <div className="pl-4">
+            <button
+              className="text-blue-700 font-semibold rounded-full border
+              border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default home;
+export default Home;
