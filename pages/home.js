3660b5d3258b63ed182c982b2ea47b4e1fbee1c7
seo722 <95078290+seo722@users.noreply.github.com>
2022-04-15 13:18:44 +0900

    home page

--- pages/home.js
+++ pages/home.js
@@ -9,7 +9,7 @@ import Example from "../components/Header";
 
 function Home() {
   return (
-    <div>
+    <div className="space-y-10 relative">
       <header className="flex justify-around items-center py-4">
         <div className="relative w-36 h-10">
           <Image src="http://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
@@ -31,6 +31,34 @@ function Home() {
           </div>
         </div>
       </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+        <div className="space-y-6 xl:space-y-10">
+          <h1
+            className="text-3xl md:text-5xl text-amber-800 max-w-xl !leading-snug
+          pl-4 xl:pl-0"
+          >
+            Welcome to your professional community
+          </h1>
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Find a person you know</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+        <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5">
+          <Image src="https://rb.gy/vkzpzt" layout="fill" priority />
+        </div>
+      </main>
     </div>
   );
 }
