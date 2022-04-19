115a1d846724326d05affd98e15abb3f7b6ae545
seo722 <95078290+seo722@users.noreply.github.com>
2022-04-19 22:47:53 +0900

    middleware

--- pages/home.js
+++ pages/home.js
@@ -7,8 +7,9 @@ import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import Example from "../components/Header";
 import Head from "next/head";
+import { getProviders, signIn } from "next-auth/react";
 
-function Home() {
+function Home({ providers }) {
   return (
     <div className="space-y-10 relative">
       <Head>
@@ -25,14 +26,18 @@ function Home() {
             <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
             <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
           </div>
-          <div className="pl-4">
-            <button
-              className="text-blue-700 font-semibold rounded-full border
-              border-blue-700 px-5 py-1.5 transition-all hover:border-2"
-            >
-              Sign in
-            </button>
-          </div>
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <div className="pl-4">
+                <button
+                  className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+                  onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                >
+                  Sign in
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </header>
 
@@ -68,3 +73,13 @@ function Home() {
 }
 
 export default Home;
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
